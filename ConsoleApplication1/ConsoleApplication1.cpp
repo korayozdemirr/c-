d50eb08acb255b3620fd66512216167c6383d781
korayozdemirr <koray.ozdemir11@gmail.com>
2023-03-11 18:32:45 +0300

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿// ConsoleApplication1.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+
+	return 0;
+}
---
+++ ConsoleApplication1/ders2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int ders2() {
+
+	cout << "ders 2" << endl;
+	cout << "ders 3";
+
+	return 0;
+}
